5f3facadfbe37818ccf2c90cdd2e8b89a42487d6
Kristina <87947917+KrisssMi@users.noreply.github.com>
2022-03-01 23:40:27 +0300

    ADD

--- Лабораторная №3/08.sql
+++ Лабораторная №3/08.sql
@@ -2,17 +2,18 @@
 /*Внесение изменений в сценарий создания базы данных с тем, чтобы файлы размещались в определенных местах памяти.*/
 
 use master  
-create database MiKV_bd on primary	--первичная файловая группа
-( name = N'MiKV_bd_mdf', filename = N'E:\BD\new\MiKV_bd_mdf.mdf', 
-   size = 10240Kb, maxsize=UNLIMITED, filegrowth=1024Kb),
+create database MiKV_bd on primary									--первичная файловая группа
+( name = N'MiKV_bd_mdf', filename = N'E:\BD\new\MiKV_bd_mdf.mdf',	--логическое имя файла, имя файла в ОС
+   size = 10240Kb, maxsize=UNLIMITED, filegrowth=1024Kb),			--первоначальный размер  файла, максимальный размер  файла и приращение
+
 ( name = N'MiKV_bd_ndf', filename = N'E:\BD\new\MiKV_bd_ndf.ndf', 
    size = 10240KB, maxsize=1Gb, filegrowth=25%),
-filegroup FG1							--вторичная файловая группа
+filegroup FG1														--вторичная файловая группа
 ( name = N'MiKV_bd_fg1_1', filename = N'E:\BD\new\MiKV_bd_fgq-1.ndf', 
    size = 10240Kb, maxsize=1Gb, filegrowth=25%),
 ( name = N'MiKV_bd_fg1_2', filename = N'E:\BD\new\MiKV_bd_fgq-2.ndf', 
    size = 10240Kb, maxsize=1Gb, filegrowth=25%)
-log on
+log on																--журнал транзакций
 ( name = N'MiKV_bd_log', filename=N'E:\BD\new\MiKV_bd_log.ldf',       
    size=10240Kb,  maxsize=2048Gb, filegrowth=10%)
 go
